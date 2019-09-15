a75e77b1ec20fc350a24dd3a86ca399227021493
Thai Nguyen <discoverylight@yahoo.com>
2019-09-15 13:14:29 -0700

    removed redundancy codes in server.js

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 
-const projectRouter = require("./resources/projects/project.controller");
-
 const server = express();
 server.use(express.json());
 server.use(morgan("dev"));
-
-//server.use("/api/projects", projectRouter);
 
 const serverRoutes = require("./routes")(server);
 
